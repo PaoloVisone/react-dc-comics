a772b8759a286b3eda3ecfd14a6d289b431425e7
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-12 18:16:19 +0100

    [feat] import footer and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 // Importo il componetnte Header
 import Header from "./components/Header.jsx";
 
+// Importo il componetnte Main
+import Main from "./components/Main.jsx";
+
+// Importo il componetnte Footer
+import Footer from "./components/Footer.jsx";
+
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
-
+      <Main />
+      <Footer />
     </>
   )
 }
