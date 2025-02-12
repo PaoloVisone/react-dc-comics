acedf36975e254bd5af6b999579ffdf5d7999c28
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-12 17:27:54 +0100

    [feat] add nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,23 @@
 export default function Header() {
-    return (<header> /* contenuto qui */ </header>);
+    return (<header>
+        <figure id="logo">
+            <img src="img/dc-logo.png" alt="logo" />
+        </figure>
+
+        <nav id="nav-menu">
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAME</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+
+    </header>);
 }
