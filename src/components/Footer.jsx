c5be4cc4b1e1be5abb4c6cb738eeb77ce5afe1da
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-13 16:29:50 +0100

    [feat] add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,112 @@
 export default function Footer() {
-    return (<footer> /* contenuto qui */ </footer>);
+    return (<footer>
+
+        {/* Container */}
+        <div className="container flex">
+
+            <div id="content-list">
+
+                {/* Footer List */}
+                <div id="foot-list">
+
+                    <ul>
+                        <li><a className="big" href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                    <ul>
+                        <li><a className="big" href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+
+                </div>
+
+                <div id="foot-list">
+
+                    <ul>
+                        <li><a className="big" href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+
+                </div>
+
+                <div id="foot-list">
+
+                    <ul>
+                        <li><a className="big" href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            {/* Logo Big */}
+            <div id="foot-img">
+
+                <figure>
+                    <img id="logo-bg" src="img/dc-logo-bg.png" alt="" />
+                </figure>
+
+            </div>
+
+        </div>
+
+        {/* Footer Navbar */}
+        <div id="foot-navbar">
+
+            <div className="container flex">
+
+                {/* Button */}
+                <div>
+
+                    <button id="btn">SIGN-UP NOW</button>
+
+                </div>
+
+
+                <div id="icon">
+
+                    {/* Text Follow */}
+                    <p id="foot-text">FOLLOW US</p>
+
+                    {/* Icon */}
+                    <figure>
+                        <img src="img/footer-facebook.png" alt="facebook" />
+                    </figure>
+                    <figure>
+                        <img src="img/footer-periscope.png" alt="periscope" />
+                    </figure>
+                    <figure>
+                        <img src="img/footer-pinterest.png" alt="pinterest" />
+                    </figure>
+                    <figure>
+                        <img src="img/footer-twitter.png" alt="twitter" />
+                    </figure>
+                    <figure>
+                        <img src="img/footer-youtube.png" alt="youtube" />
+                    </figure>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </footer>);
 }
