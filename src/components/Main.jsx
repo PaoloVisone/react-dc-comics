4369f1b15f614b677253b24106175425b0691c77
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-13 09:08:46 +0100

    [feat] add main elements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,39 @@
 export default function Main() {
-    return (<main> /* contenuto qui */ </main>);
+    return (<main>
+        <div className="container flex">
+            <div className="card">
+                <figure>
+                    <img className="main-img" src="img\buy-comics-digital-comics.png" alt="digital" />
+                </figure>
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div className="card">
+                <figure>
+                    <img className="main-img" src="img\buy-comics-merchandise.png" alt="merchandise" />
+                </figure>
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div className="card">
+                <figure>
+                    <img className="main-img" src="img\buy-comics-shop-locator.png" alt="shop-locator" />
+                </figure>
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div className="card">
+                <figure>
+                    <img className="main-img" src="img\buy-comics-subscriptions.png" alt="subscription" />
+                </figure>
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div className="card">
+                <figure>
+                    <img className="main-img" src="img\buy-dc-power-visa.svg" alt="power-visa" />
+                </figure>
+                <p>DIGITAL COMICS</p>
+            </div>
+        </div>
+
+
+
+    </main>);
 }
