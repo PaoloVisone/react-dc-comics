e63369fd7cb3b6bb80ecac3b02db17b11da3700e
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-13 09:06:23 +0100

    [feat] add container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,24 @@
 export default function Header() {
     return (<header>
-        <figure id="logo">
-            <img src="img/dc-logo.png" alt="logo" />
-        </figure>
+        <div className="container flex">
+            <figure id="logo">
+                <img src="img/dc-logo.png" alt="logo" />
+            </figure>
 
-        <nav id="nav-menu">
-            <ul>
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAME</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
-            </ul>
-        </nav>
-
+            <nav>
+                <ul id="nav-menu">
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAME</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>);
 }
