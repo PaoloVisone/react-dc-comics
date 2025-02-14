4c2688e5fbd0d86f6111a0ff1a05887e8815e565
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-14 16:17:09 +0100

    [feat] add  "Hero" component

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import Main from "./components/Main.jsx";
 // Importo il componetnte Footer
 import Footer from "./components/Footer.jsx";
 
+// Importo il componetnte Hero
+import Hero from "./components/Hero.jsx";
+
 import './App.css'
+
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Main />
       <Footer />
     </>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+
+        <div id="hero">
+            {/* <button id="banner-btn">Ciao</button> */}
+        </div>
+    );
+}
