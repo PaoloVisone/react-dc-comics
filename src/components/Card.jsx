a1b4b24d2678340710057b9fcad196bf5aed0cd2
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-14 17:40:36 +0100

    [feat] add component "Card"

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,26 @@
-export default function Card() {
-    const cardElements = [
-        { id: 1, src: '../img/buy-comics-digital-comics.png', alt: "digital", text: "DIGITAL COMICS" },
-        { id: 2, src: "../img/buy-comics-merchandise.png", alt: "merchandise", text: "DC MERCHANDISE" },
-        { id: 3, src: "../img/buy-comics-subscriptions.png", alt: "subscription", text: "COMIC SHOP LOCATOR" },
-        { id: 4, src: "../img/buy-comics-shop-locator.png", alt: "shop-locator", text: "SUBSCRIPTION" },
-        { id: 5, src: "../img/buy-dc-power-visa.svg", alt: "power-visa", text: "DC POWER VISA" }
-    ];
-    return (
-        <div className="container flex">
+// Importo comics.js
+import comics from "/comics.js";
 
-            {/* Itero gli elementi */}
-            {cardElements.map((cardElement) => (
+console.log(comics);
+
+
+export default function Card() {
+    return (
+
+        <div className="container flex wrap">
+
+            {comics.map((comic) => (
 
                 <div className="card">
 
-                    {/* Elementi iterati */}
-                    <figure>
-
-                        {/* Immaagine della card */}
-                        <img className="main-img" key={cardElement.id} src={cardElement.src} alt={cardElement.alt} />
-
-                    </figure>
-
-                    {/* Testo */}
-                    <p>{cardElement.text}</p>
+                    <img key={comic.id} src={comic.thumb} alt={comic.series} />
+                    <p className="text-img">{comic.series}</p>
 
                 </div >
 
-            ))}
+            ))
+            }
 
-        </div>
-
-    )
+        </div >
+    );
 }
