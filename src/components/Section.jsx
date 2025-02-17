201579cc8df1563e7c29debbddfc5f1ee3f07f71
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-17 15:08:03 +0100

    [feat] add component "Section"

---
+++ src/components/Section.jsx
@@ -0,0 +1,21 @@
+// Importo le card
+import Card from "./Card";
+
+
+export default function Section() {
+    return (<section>
+
+        <Card />
+
+
+        {/* Button */}
+        <div className="content-btn dir-ctr">
+
+            <button className="btn blu">LOAD MORE</button>
+
+        </div>
+
+
+
+    </section>);
+}
